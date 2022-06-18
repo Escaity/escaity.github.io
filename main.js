1d4d20c017b6db1595c67752ea1bed8e8d07ead8
Escaity <sfz2306@gmail.com>
2022-06-19 00:22:19 +0900

    変化色の追加

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ const h1Click = document.getElementById('dice');
 h1Click.addEventListener('click', () => {
   const list = document.getElementsByTagName('li');
   Array.prototype.forEach.call(list, (key) => {
-    const rand = Math.ceil(Math.random() * 6);
+    const rand = Math.ceil(Math.random() * 10);
 
     key.textContent = '★';
     key.setAttribute('id', 'd' + rand.toString());
