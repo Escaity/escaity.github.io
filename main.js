f8aadd28be13b673b4b6e43fb4b42a98804dba4a
Escaity <sfz2306@gmail.com>
2022-06-18 23:29:35 +0900

    サイコロを星に変更

--- main.js
+++ main.js
@@ -3,7 +3,8 @@ h1Click.addEventListener('click', () => {
   const list = document.getElementsByTagName('li');
   Array.prototype.forEach.call(list, (key) => {
     const rand = Math.ceil(Math.random() * 6);
-    key.textContent = rand;
+
+    key.textContent = '★';
     key.setAttribute('id', 'd' + rand.toString());
   });
 });
