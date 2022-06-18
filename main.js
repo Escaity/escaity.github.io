f88f3018d89e1005fe0a74fe0b7d0aa7435a7d91
Escaity <sfz2306@gmail.com>
2022-06-18 23:18:59 +0900

    出た目のアニメーションを追加

--- main.js
+++ main.js
@@ -2,6 +2,8 @@ const h1Click = document.getElementById('dice');
 h1Click.addEventListener('click', () => {
   const list = document.getElementsByTagName('li');
   Array.prototype.forEach.call(list, (key) => {
-    key.textContent = Math.ceil(Math.random() * 6);
+    const rand = Math.ceil(Math.random() * 6);
+    key.textContent = rand;
+    key.setAttribute('id', 'd' + rand.toString());
   });
 });
